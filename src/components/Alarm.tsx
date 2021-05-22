6ca30536d666665c13ce9af14886db1e2014c435
coder wessel <nospam917+github@gmail.com>
2021-05-22 14:54:29 +0200

    pause button with sound

--- src/components/Alarm.tsx
+++ src/components/Alarm.tsx
@@ -11,39 +11,4 @@ const Alarm: React.FC<IAlarmProps> = ({ audioRef }) => {
   return <audio id="beep" ref={audioRef} src={AudioBeepSound} />;
 };
 
-// type IAlarmProps = {
-//   active: boolean;
-//   handleEnd(): void;
-//   handleStart(): void;
-//   //use handeler function if desired
-// };
-
-// const Alarm: React.FC<IAlarmProps> = ({ active, handleEnd, handleStart }) => {
-//   const audioRef: React.RefObject<HTMLAudioElement> = React.createRef();
-//   const playAudio = () => {
-//     const loadedAudio = audioRef.current;
-//     if (active && loadedAudio) {
-//       loadedAudio.loop = false;
-//       loadedAudio.addEventListener("ended", handleEnd);
-//       try {
-//         loadedAudio.play();
-//       } catch (Err) {
-//         alert("audio broken");
-//       } finally {
-//         return;
-//       }
-//     } else if (loadedAudio) {
-//       loadedAudio.pause();
-//     }
-//   };
-//   React.useEffect(() => playAudio());
-//   return (
-//     <audio
-//       id="beep"
-//       ref={audioRef}
-//       src={process.env.PUBLIC_URL + "./AudioBeepSound.wav"}
-//     />
-//   );
-// };
-
 export default Alarm;
--- src/components/PausePlayButton.tsx
+++ src/components/PausePlayButton.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import "./PausePlayButton.css";
+import PausePlaySound from "../assets/play-pause.mp3";
 //use reducer tools
 
 type IPausePlayButtonProps = {
@@ -7,15 +8,42 @@ type IPausePlayButtonProps = {
   handler(): void;
 };
 const PausePlayButton: React.FC<IPausePlayButtonProps> = ({ handler }) => {
+  const pausePlayAudioRef: React.RefObject<HTMLAudioElement> =
+    React.createRef();
+
+  const playAudio = async () => {
+    const loadedAudio = pausePlayAudioRef.current;
+    if (loadedAudio) {
+      loadedAudio.loop = false;
+      //loadedAudio.addEventListener("ended", handleAlarmEnd);
+      try {
+        loadedAudio.play();
+      } catch (Err) {
+        alert("audio broken");
+      } finally {
+        return;
+      }
+    }
+  };
+
+  const pausePlayHandler = () => {
+    playAudio();
+    handler();
+  };
+
   return (
-    <button
-      id="start_stop"
-      className="btn col-12 btn-primary"
-      onClick={handler}
-    >
-      <i className="bi bi-play-circle-fill" />
-      <i className="bi bi-pause-circle-fill" />
-    </button>
+    <div>
+      <button
+        id="start_stop"
+        className="btn col-12 btn-primary"
+        onClick={() => pausePlayHandler()}
+      >
+        <i className="bi bi-play-circle-fill" />
+        <i className="bi bi-pause-circle-fill" />
+      </button>
+      <audio id="pauseplaysound" ref={pausePlayAudioRef} src={PausePlaySound} />
+      ;
+    </div>
   );
 };
 export default PausePlayButton;
